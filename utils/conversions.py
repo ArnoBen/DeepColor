fa18762cee7186579b7e1c7a0aabd8251823bf1a
Arno Benizri <arno.benizri@gmail.com>
2022-11-04 14:08:00 +0200

    [FIX] comments were wrong

--- utils/conversions.py
+++ utils/conversions.py
@@ -8,14 +8,14 @@ def bgr2rgb(img):
 
 def rgb2lab(img):
     result = cv2.cvtColor(img, cv2.COLOR_RGB2LAB).astype(np.float32)
-    result[:, :, 0] *= 100/255 # (0, 100) => (0, 255)
+    result[:, :, 0] *= 100/255 # (0, 255) => (0, 100)
     result[:, :, 1:] -= 128  # (0, 255) => (-127, 127)
     return result.astype(np.int8)
 
 
 def lab2rgb(img):
     img = img.astype(np.float32)
-    img[:, :, 0] *= 255/100  # (0, 255) => (0, 100)
+    img[:, :, 0] *= 255/100  # (0, 100) => (0, 255)
     img[:, :, 1:] += 128  # (-127, 127) => (0, 255)
     return cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_LAB2RGB)
 
