683a59024b1bd8a7abc2fa080f4e772d42edde77
Arno Benizri <arno.benizri@gmail.com>
2022-11-05 15:32:01 +0200

    [FIX] Folder extraction [UPDATE] resizing during extraction

--- bash/download_extract_coco.sh
+++ bash/download_extract_coco.sh
@@ -5,18 +5,19 @@ wget http://images.cocodataset.org/zips/$COCO_DATASET.zip
 
 # Extracting files into subdirectories
 filenames=($(7z l test2017.zip|grep .jpg|awk -F' ' '{print $6}'))
-n_files = ${#filenames[@]}
-for i in {1..30}
+n_files=${#filenames[@]}
+for i in {1..40}
 do
     splice=$((i*1000))
     subdir=$splice
-    if ((i < 10))
-    then
-      subdir="0${splice}"
+    if ((i < 10)); then subdir="0${splice}"; fi
+    
+    if ((i < 30)); then set="train"
+    elif ((i < 35)); then set="valid"
+    else set="test"
     fi
-    echo $subdir
-    7z e test2017.zip -oimages/train/$subdir ${filenames[*]:splice-1000:splice}
-done
 
-7z e test2017.zip -oimages/valid/ ${filenames[*]:30000:35000}
-7z e test2017.zip -oimages/test/ ${filenames[*]:35000:n_files}
+    echo $subdir
+    7z e test2017.zip -oimages/$set/$subdir ${filenames[*]:(($splice-1000)):1000}
+    mogrify -resize 256x256! images/$set/$subdir/*.jpg
+done
