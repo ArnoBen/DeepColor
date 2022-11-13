602da959069437a3a07d756b7d2fa444a380ac63
Kseniia Konoshko <ksushka.k.393@gmail.com>
2022-11-13 16:23:14 +0200

    modified:   utils/models.py

--- utils/models.py
+++ utils/models.py
@@ -1,5 +1,5 @@
-from keras.layers import Conv2D, UpSampling2D, InputLayer
-from keras.models import Sequential
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Input, RepeatVector, Reshape, concatenate
+from keras.models import Sequential, Model
 
 # Baseline model
 
@@ -44,3 +44,31 @@ def get_model(model_size: str):
         model.add(UpSampling2D((2, 2)))
     
     return model
+
+def get_full_model():
+    embed_input = Input(shape=(1000,))
+    #Encoder
+    encoder_input = Input(shape=(256, 256, 1,))
+    encoder_output = Conv2D(64, (3,3), activation='relu', padding='same', strides=2)(encoder_input)
+    encoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(128, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same', strides=2)(encoder_output)
+    encoder_output = Conv2D(512, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(512, (3,3), activation='relu', padding='same')(encoder_output)
+    encoder_output = Conv2D(256, (3,3), activation='relu', padding='same')(encoder_output)
+    #Fusion
+    fusion_output = RepeatVector(32 * 32)(embed_input) 
+    fusion_output = Reshape(([32, 32, 1000]))(fusion_output)
+    fusion_output = concatenate([encoder_output, fusion_output], axis=3) 
+    fusion_output = Conv2D(256, (1, 1), activation='relu', padding='same')(fusion_output)
+    #Decoder
+    decoder_output = Conv2D(128, (3,3), activation='relu', padding='same')(fusion_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = Conv2D(64, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    decoder_output = Conv2D(32, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = Conv2D(16, (3,3), activation='relu', padding='same')(decoder_output)
+    decoder_output = Conv2D(2, (3, 3), activation='tanh', padding='same')(decoder_output)
+    decoder_output = UpSampling2D((2, 2))(decoder_output)
+    return Model(inputs=[encoder_input, embed_input], outputs=decoder_output)
